c0d6313ce27cd1f697c1861354f4dd694378b267
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-04-22 21:03:33 -0300

    cambios en el tvNoise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React ,{useEffect, useState} from 'react'
 
 
-import TVNoise from './components/tvNoise'
+
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
