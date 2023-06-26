f4f053a2e1eb4ff178711a132cf8cbb474e7942c
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-06-26 16:17:02 -0300

    cambios hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import videoHero from "../assets/reel.mp4";
 
 function Hero() {
   return (
-    <div className=" flex justify-center items-center h-screen hero p-10 pb-20">
+    <div className=" flex justify-center items-center sm:h-screen  hero p-10 pb-20">
       <div className="max-w-[1220px]  rounded-lg p-1 bg-[#4A6D69] bloq_color  ">
         <video className=" rounded-lg  " src={videoHero} autoPlay loop muted />
       </div>
