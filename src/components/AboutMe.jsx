9d5e3a61e0cf4f9477ce93dc9b770b47680037d4
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-06-26 18:52:57 -0300

    cambios en el grid y el hero

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
   return (
     <div className='bg-black sm:p-16'>
       <h1 className='text-white text-4xl sobre_mi'>Sobre Mi</h1>
-        <p className='text-white p-6'>
+        <p className='text-white p-6 pt-1'>
         Carla Giuliani egresada de la carrera de Diseño de Imagen y Sonido de la Universidad de Buenos Aires (UBA) y oriunda de la ciudad de San Fernando desarrolla su carrera en el ámbito audiovisual como realizadora, editora y coordinadora de postproducción. En algunas ocasiones también se desempeña como continuista y asistente de dirección. <br /> <br />
 En mayo de 2018, se estrena su primer largometraje como editora titulado "El Corte". La película que surge como tesis de su carrera se proyecta en Cine Gaumont logrando permanencia en pantalla durante dos semanas. Así mismo, recorre varias salas del espacio INCAA como San Justo, La Matanza y Morón. <br /> <br />
 Carla colabora como continuista en proyectos cortometrajes como “El Medium” de Matías Marra, seleccionado en BAFICI 2021  y “Ecdisis Corpórea” 2022 de Cristian Bidone donde también se desempeña como editora. <br /> <br />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import videoHero from "../assets/reel.mp4";
 
-function Hero() {
+const Hero = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [countdown, setCountdown] = useState(0);
+
+  useEffect(() => {
+    const video = document.createElement("video");
+
+    video.src = videoHero;
+    video.preload = "metadata";
+
+    const handleLoadedMetadata = () => {
+      const duration = Math.ceil(video.duration);
+      setCountdown(duration);
+      setIsLoading(false);
+    };
+
+    video.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      video.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, []);
+
   return (
-    <div className=" flex justify-center items-center sm:h-screen  hero p-10 pb-20">
-      <div className="max-w-[1220px]  rounded-lg p-1 bg-[#4A6D69] bloq_color  ">
-        <video className=" rounded-lg  " src={videoHero} autoPlay loop muted />
-      </div>
+    <div className="flex justify-center items-center sm:h-screen hero p-10 pb-20">
+      {isLoading ? (
+        <div className="max-w-[1220px] rounded-lg p-1 bg-[#4A6D69] bloq_color">
+          <div className="text-white">Cargando...</div>
+          
+        </div>
+      ) : (
+        <div className="max-w-[1220px] rounded-lg p-1 bg-[#4A6D69] bloq_color">
+          <video className="rounded-lg" src={videoHero} autoPlay loop muted />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Hero;
