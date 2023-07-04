f8c079c00194eff09f2a9e64768a8d9446f734bd
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-07-04 16:08:12 -0300

    cambios en el hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
 
   return (
     <div className="bg-black h-screen flex items-center justify-center">
-      {!videoLoaded && <div className="text-white">Loading</div>}
+      {!videoLoaded && <div className="text-white text-center sm:text-4x1">Loading</div>}
       <div className={`${videoLoaded ? "opacity-100" : "opacity-0"} transition-opacity duration-500`}>
         <video
           id="videoHero"
